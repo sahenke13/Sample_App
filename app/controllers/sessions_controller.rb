1d95881fab00e2f7b632fa4ac4ab9909ed9db3b9
Steven Henke <steven@foundrymakes.com>
2019-07-01 08:11:23 -0500

    adding advanced login techniques

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user
       redirect_to user
       #log the user in and ridirect to the user's show page
     else  
