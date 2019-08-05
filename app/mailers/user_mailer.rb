909ed38178249b654be38ec744594c68daeec1d8
Steven Henke <steven@foundrymakes.com>
2019-08-04 20:16:25 -0500

    adds account activation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,7 @@ class UserMailer < ApplicationMailer
   #
   def account_activation(user)
     @user = user
-    email to: user.email, subject: "account_activation"
+    mail to: user.email, subject: "Account activation"
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
